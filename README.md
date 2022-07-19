1724a4b44e200fd9c2f6bbd5291b23e568a6b20f
羅雲聘 <pzps94038@yahoo.com.tw>
2022-07-19 21:31:07 +0800

    fix:修改docker配置檔、新增git忽略文件

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # Charlotte
 # frontend(Angular)
+rxjs
 ngx-spinner
 Angular Material
 ngx-sweetalert2
 ng-boostarp
 # backend(C#)
+
+
+docker指令
+後端打包
+cd 專案路徑\Charlotte\backend\Charlotte
+docker build -t pzps94038/charlotte-backend .
+
+docker-componse
+cd 專案路徑
+docker-componse up -d
---
+++ backend/Charlotte/Charlotte/Controllers/TestController.cs
@@ -0,0 +1,44 @@
+﻿using Charlotte.Enum;
+using Charlotte.Helper.Login;
+using Charlotte.Interface.Shared;
+using Charlotte.Model;
+using Charlotte.Model.Shared;
+using Charlotte.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+
+namespace Charlotte.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class TestController : ControllerBase
+    {
+
+        /// <summary>
+        /// 測試連線用
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ResultModel Test() 
+        {
+            ResultModel result = new ResultModel();
+            try
+            {
+                LoggerUtils.Info("測試寫入");
+                result.Code = HttpStatusCode.OK;
+                result.Message = EnumUtils.GetDescription(EnumResult.Success);
+            }
+            catch (Exception ex) 
+            {
+                result.Code = HttpStatusCode.BadRequest;
+                result.Message = EnumUtils.GetDescription(EnumResult.Fail);
+                LoggerUtils.Error(ex);
+            }
+            return result;
+        }     
+    }
+}
