73781ab536ab2c0ab2b50d1afe52ec7c994d7a61
Ron <pzps94038@yahoo.com.tw>
2022-08-27 02:34:21 +0800

    fix: 調整介面

--- backend/Charlotte/Charlotte/Interface/ProductType/IProductTypeHelper.cs
+++ backend/Charlotte/Charlotte/Interface/ProductType/IProductTypeHelper.cs
@@ -1,6 +1,11 @@
-﻿namespace Charlotte.Interface.ProductType
+﻿using Charlotte.Interface.Shared;
+using Charlotte.Model.ProductType;
+using Charlotte.VModel.ManagerProductType;
+using Charlotte.VModel.Shared;
+
+namespace Charlotte.Interface.ProductType
 {
-    public class IProductTypeHelper
+    public interface IProductTypeHelper: IGetAllAsync<TableVModel<MnaagerProductTypeVModel>>, IDeleteAsync, IBatchDeleteAsync, IModifyAsync<ManagerProductTypeModel>, ICreateAsync<ManagerProductTypeModel>
     {
     }
 }
